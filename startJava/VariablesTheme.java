82cfb9334ec3fb4cd49c021a046e818c1d184adf
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-08-30 01:12:44 +0300

    add task1

---
+++ startJava/VariablesTheme.java
@@ -0,0 +1,13 @@
+public class VariablaesTheme {
+    public static void main(String[] args) {
+        System.out.println("1. Создание переменных и вывод их значений на консоль\n");
+        byte core = 6;
+        short frequency1 = 4000;
+        int frequency2 = 4000000000;
+        long numKeys = 12345678012345678;
+        float price 45000.230;
+        double pi = 3.141592653;
+        char letter = 's';
+        bolean bool = true;
+    }
+}
