a07cdaa9526dcb74e5d3b05a80984b39c40933ad
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-09-09 21:38:23 +0300

    add final task;

--- startJava/Lesson_1/MyFirstGame.java
+++ startJava/Lesson_1/MyFirstGame.java
@@ -1,9 +1,37 @@
 import java.util.Random;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class MyFirstApp {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Random rnd = new Random();
-        int operationNumber = rnd.nextInt(100);
+        int hiddenNum = rnd.nextInt(99) + 1;
+        int inputNum = 0;
+        int count = 0;
+        boolean gameOver = false;
+        do {
+            boolean numIsCorrect = false;
+            while(numIsCorrect == false) {
+                try {
+                    System.out.print("Введите целое число: ");
+                    String num = scanner.nextLine();
+                    inputNum = Integer.parseInt(num);
+                    numIsCorrect = true;
+                    } catch (Exception e) {
+                        System.out.println("Введите корректно число\n");
+                    }
+            }
+            count++;
+            if(inputNum > hiddenNum) {
+                System.out.println("\nЧисло " + inputNum + " больше того, что загадал компьютер\n");
+            } else if(inputNum < hiddenNum) {
+                System.out.println("\nЧисло " + inputNum + " меньше того, что загадал компьютер\n");
+            } else {
+                gameOver = true;
+                System.out.println("\nВы угадали за " + count + " попыток. Загаданное число " + hiddenNum);
+            }
+        } while(gameOver == false);
     }
 }
