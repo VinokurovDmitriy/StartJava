c786c1cdfb7c03a1994431280c0255e4ad88a834
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-09-19 23:01:01 +0300

    lesson2 task3 add

--- startJava/Lesson_2/WolfTest.java
+++ startJava/Lesson_2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf jack = new Wolf();
-        jack.gender = "male";
-        jack.name = "Jack";
-        jack.weight = 35.32f;
-        jack.age = 4;
-        jack.color = "gray";
-        System.out.println("Wolf name is " + jack.name);
-        System.out.println("Sex wolf " + jack.gender);
-        System.out.println("Wolf " + jack.age + " years old");
-        System.out.println("Wolf weight " + jack.weight);
-        System.out.println("Wolf color " + jack.color);
+        jack.setGender("male");
+        jack.setName("Jack");
+        jack.setWeight(35.32f);
+        jack.setAge(4);
+        jack.setColor("gray");
+        System.out.println("Wolf name is " + jack.getName());
+        System.out.println("Sex wolf " + jack.getGender());
+        System.out.println("Wolf " + jack.getAge() + " years old");
+        System.out.println("Wolf weight " + jack.getWeight());
+        System.out.println("Wolf color " + jack.getColor());
         jack.go();
         jack.run();
         jack.hunt();
