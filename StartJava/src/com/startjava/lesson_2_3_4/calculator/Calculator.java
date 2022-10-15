9f1086cf7a53061788b5ed1061053e7a946e1d46
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-10-15 19:45:46 +0300

    lesson4 fix task1*

--- StartJava/src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ StartJava/src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,34 +1,16 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.net.ServerSocket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
 
     public static int calculate(String mathExpression) {
-        String[] expressionArr = mathExpression.split(" ");
-        boolean correct = false;
-        int num1 = 0;
-        int num2 = 0;
-        while(!correct) {
-            try {
-                num1 = Integer.parseInt(expressionArr[0]);
-                num2 = Integer.parseInt(expressionArr[2]);
-                if(num1 > 0 && num2 > 0) {
-                    correct = true;
-                } else {
-                    System.out.print("Введите положительные целые числа: ");
-                }
-            } catch (NumberFormatException e) {
-                System.out.print("Введите целые числа: ");
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.print("Введите число знак и второе число через пробел: ");
-            }
-            if(!correct) {
-                expressionArr = (new Scanner(System.in)).nextLine().split(" ");
-            }
-        }
-        char operation = expressionArr[1].charAt(0);
+        String[] partsExpression = splitInput(mathExpression);
+        char operation = partsExpression[1].charAt(0);
+        int num1 = getNum(partsExpression, 0);
+        int num2 = getNum(partsExpression, 2);
         return switch (operation) {
             case '+' -> Math.addExact(num1, num2);
             case '-' -> Math.subtractExact(num1, num2);
@@ -38,5 +20,27 @@ public class Calculator {
             case '%' -> num1 % num2;
             default -> 0;
         };
+    }
+
+    public static String checkExpression(String mathExpression) {
+        String[] partsExpression = splitInput(mathExpression);
+        try {
+            int num1 = getNum(partsExpression, 0);
+            int num2 = getNum(partsExpression,2);
+            return (num1 > 0 && num2 > 0) ?
+                    "success" : "Введите положительные целые числа: ";
+        } catch (NumberFormatException e) {
+            return "Введите целые числа: ";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return "Введите число знак и второе число через пробел: ";
+        }
+    }
+
+    private static String[] splitInput(String input) {
+        return input.split(" ");
+    }
+
+    private static int getNum(String[] parts, int index) {
+        return Integer.parseInt(parts[index]);
     }
 }
--- StartJava/src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ StartJava/src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -7,17 +7,30 @@ import java.lang.StringIndexOutOfBoundsException;
 
 public class CalculatorTest {
 
+    static final String SUCCESS = "success";
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
         String reply = "yes";
         while(!reply.equals("no")) {
             if(reply.equals("yes")) {
                 System.out.print("Введите математическое выражение: ");
-                String mathExpression = scanner.nextLine();
+                String mathExpression = getMatExpression(console);
                 System.out.println(mathExpression + " = " + Calculator.calculate(mathExpression));
             }
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            reply = scanner.nextLine();
+            reply = console.nextLine();
         }
+    }
+
+    private static String getMatExpression(Scanner console) {
+        String correct = "";
+        String mathExpression = "";
+        while(!correct.equals(SUCCESS)) {
+            mathExpression = console.nextLine();
+            correct = Calculator.checkExpression(mathExpression);
+            if(!correct.equals(SUCCESS)) System.out.print(correct);
+        }
+        return mathExpression;
     }
 }
