86a7bcd76e7818dd29d53e83edf91782d30bc00a
VinokurovDmitriy <dnitvitvin@gmail.com>
2022-11-08 22:55:02 +0300

    fix

--- src/com/startjava/Lesson_5/init_db.sql
+++ src/com/startjava/Lesson_5/init_db.sql
@@ -1,10 +1,10 @@
 INSERT INTO jaegers VALUES ('Horizon Brave', 'Mark-1', 238, 7.890, 'Destroyed', 'China', '2015-12-22', 2),
-('Gipsy Danger', 'Mark-3', 260, 1,980, 'Destroyed', 'United States of America', '2017-06-17', 9),
-('Gipsy Avenger', 'Mark-6', 268, 2,004, 'Destroyed', 'Pan Pacific Defense Corps', '2034-01-12', 1),
-('Apex', 'Mark-5', 250, 1,850, null, 'China', '2033', 2),
-('Striker Eureka', 'Mark-5', 250, 1,850, 'Destroyed', 'Australia', '2015-11-02', 11),
-('Cherno Alpha', 'Mark-1', 280, 2,412, 'Destroyed', 'Russia', '2015-12-22', 6),
+('Gipsy Danger', 'Mark-3', 260, 1.980, 'Destroyed', 'United States of America', '2017-06-17', 9),
+('Gipsy Avenger', 'Mark-6', 268, 2.004, 'Destroyed', 'Pan Pacific Defense Corps', '2034-01-12', 1),
+('Apex', 'Mark-5', 250, 1.850, null, 'China', '2033', 2),
+('Striker Eureka', 'Mark-5', 250, 1.850, 'Destroyed', 'Australia', '2015-11-02', 11),
+('Cherno Alpha', 'Mark-1', 280, 2.412, 'Destroyed', 'Russia', '2015-12-22', 6),
 ('Obsidian Fury', 'Mark-5', 223, 2.543, 'Destroyed', 'China', '2033', 3),
-('Romeo Blue', 'Mark-1', 255, 7,775, null, 'United States of America', '2015-12-15', 2),
+('Romeo Blue', 'Mark-1', 255, 7.775, null, 'United States of America', '2015-12-15', 2),
 ('Acid Geisha', 'Mark-5', 204, 1.456, 'Destroyed', 'Japan', '2035-04-24', 5),
-('Coyote Tango', 'Mark-1', 280, 2,312, 'Destroyed', 'Japan', '2015-12-30', 4);
+('Coyote Tango', 'Mark-1', 280, 2.312, 'Destroyed', 'Japan', '2015-12-30', 4);
